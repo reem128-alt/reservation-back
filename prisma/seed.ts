b12db7d2a5fdaee64195f4b3b2eaca46a1eee51e
Reem <reemhasan088@gmail.com>
2026-01-13 13:40:22 +0300

    Fix bcryptjs import in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
