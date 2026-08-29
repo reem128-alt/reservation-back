24b16b95f7cf7da883e38a7a8bf5ed4e4bbc92b6
reem128-alt <reemhasan088@gmail.com>
2026-08-29 12:50:56 +0300

    add migration for current schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
 
   datasource: {
     url: env('DATABASE_URL'),
-    shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
+    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
   },
 });
